1167e462a083da6dba14768f70636ebc22050942
Natália Satie <natalia.odashima@gmail.com>
2026-01-31 14:49:34 -0300

    Add disclosure

--- app/sidebar/sidebar.tsx
+++ app/sidebar/sidebar.tsx
@@ -1,6 +1,14 @@
+"use client";
+
+import {Disclosure, DisclosureHeader, DisclosurePanel} from './disclosure/disclosure';
+
 export default function Sidebar() {
   return (
     <div style={{ width: '25%', height: '100vh', backgroundColor: 'grey' }}>
+        <Disclosure>
+            <DisclosureHeader>Files</DisclosureHeader>
+            <DisclosurePanel>Files content</DisclosurePanel>
+        </Disclosure>
     </div>
   );
 }
