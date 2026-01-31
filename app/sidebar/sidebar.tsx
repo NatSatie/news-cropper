a2171339922e0df5d4157b7cb6d3e4a2ce1b934c
Natália Satie <natalia.odashima@gmail.com>
2026-01-31 14:39:55 -0300

    Add new packages

--- app/sidebar/sidebar.tsx
+++ app/sidebar/sidebar.tsx
@@ -1,8 +1,6 @@
 export default function Sidebar() {
   return (
     <div style={{ width: '25%', height: '100vh', backgroundColor: 'grey' }}>
-      {/* content= */}
-      Hello World!
     </div>
   );
 }
