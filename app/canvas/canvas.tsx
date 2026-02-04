653dcba5e2db3e6d1214b0c08a21396ee0151762
Natália Satie <natalia.odashima@gmail.com>
2026-02-04 08:27:22 -0300

    add option to delete stage

--- app/canvas/canvas.tsx
+++ app/canvas/canvas.tsx
@@ -1,19 +1,18 @@
 'use client';
 
 import { SingleStageType, useCanvas } from '../provider/canvas';
-import stage from './stage/stage';
+import { Button } from '../sidebar/button/button';
 import Stage from './stage/stage';
 
 export default function Canvas() {
     const canvas = useCanvas();
-    console.log('Canvas render with stages:', canvas.canvas);
     return (
         <div style={{
             width: '75%',
             height: '100%',
             backgroundColor: 'grey',
             padding: '20px',
-            boxSizing: 'border-box',
+            boxSizing: 'content-box',
             display: 'flex',
             flexDirection: 'row',
             justifyContent: 'flex-start',
@@ -21,8 +20,28 @@ export default function Canvas() {
             gap: '20px',
         }}>
             {canvas.canvas.map((stage: SingleStageType) => (
-                <Stage key={stage.width + 'x' + stage.height} width={stage.width} height={stage.height} />
-            ))}
-        </div>
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                    }}>
+                    <div
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                        }}>
+                        <h2>{stage.name}</h2>
+                        <Button onClick={
+                            () => {
+                                const newCanvas = canvas.canvas.filter(s => s !== stage);
+                                canvas.setCanvas(newCanvas);
+                            }
+                        }> Delete Stage </Button>
+                    </div>
+                    <Stage key={stage.width + 'x' + stage.height} width={stage.width} height={stage.height} />
+                </div>
+            ))
+            }
+        </div >
     );
 }
